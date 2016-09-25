7cc31bf2409e0364560d076e2a30852a187b7f59
Md. Asiful Haque <kh3laghar@gmail.com>
2016-09-26 04:29:20 +0600

    TopUp IT TRAINING PRACTICE

--- src/mobileapplicationlicthomepractice/CoreJavaDay1Assignment/TaskFiveMainClass.java
+++ src/mobileapplicationlicthomepractice/CoreJavaDay1Assignment/TaskFiveMainClass.java
@@ -5,23 +5,28 @@
  */
 package mobileapplicationlicthomepractice.CoreJavaDay1Assignment;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Md.Asiful
- * @serial 1.3
- * @since 2016 September 24
- * @author asm-linksys.com
  */
-public class TaksThreeApp {
+public class TaskFiveMainClass {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+   
+        System.out.println("Please Enter Upper Value = ");
+        int firstNumber=scn.nextInt();
+        System.out.println("Please Enter Lower Value = ");
+        int secondNumber=scn.nextInt();
+        
+        TaskFiveMethod t5=new TaskFiveMethod();
+        System.out.println(t5.dividerCalc(firstNumber, secondNumber));
         // TODO code application logic here
-        TaskThreeMethod t3=new TaskThreeMethod();
-        t3.firstNumberInput();
-        //System.out.println(t3.firstNumberInput());
     }
     
 }
--- src/mobileapplicationlicthomepractice/CoreJavaDay1Assignment/TaskFiveMethod.java
+++ src/mobileapplicationlicthomepractice/CoreJavaDay1Assignment/TaskFiveMethod.java
@@ -12,16 +12,12 @@ package mobileapplicationlicthomepractice.CoreJavaDay1Assignment;
  * @since 2016 September 24
  * @author asm-linksys.com
  */
-public class TaksThreeApp {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        TaskThreeMethod t3=new TaskThreeMethod();
-        t3.firstNumberInput();
-        //System.out.println(t3.firstNumberInput());
+public class TaskFiveMethod {
+    public int dividerCalc(int upValue, int downValue)
+    {
+        int result=upValue/downValue;
+       return result;
     }
+    
     
 }
